28cb99574c750c57c318c302137f6e108da8c37b
facte sun <shems.chelgoui@gmail.com>
2023-04-26 17:55:13 +0200

    ✨ added entities and fixtures created database

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Entity\ProductCategory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public const NB_CATEGORIES = 50;
+    public const NB_PRODUCTS = 200;
+    public const CATEGORY_REF_PREFIX = 'CATEGORY_';
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= self::NB_CATEGORIES; $i++) {
+            $category = new ProductCategory();
+            $category
+                ->setName($faker->realText(15))
+                ->setDescription($faker->realTextBetween(200, 500));
+            $this->addReference('CATEGORY_' . $i, $category);
+            $manager->persist($category);
+        }
+
+
+        for($i = 0; $i < self::NB_PRODUCTS; $i++) {
+            $post = new Product();
+            $post
+                ->setName($faker->realText(35))
+                ->setVisible($faker->boolean(90))
+                ->setDescription($faker->paragraphs(6, true))
+                ->setTaxFreePrice($faker->randomFloat(2, 0, 1000))
+                ->setDiscount($faker->boolean(20))
+                ->setCategory(
+                    $this->getReference(
+                        self::CATEGORY_REF_PREFIX . $faker->numberBetween(1, self::NB_CATEGORIES)));
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $visible = true;
+
+    #[ORM\Column]
+    private ?bool $discount = false;
+
+    #[ORM\Column]
+    private ?float $taxFreePrice = null;
+
+    #[ORM\ManyToOne(inversedBy: 'Products')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ProductCategory $category = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isVisible(): ?bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): self
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    public function isDiscount(): ?bool
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(bool $discount): self
+    {
+        $this->discount = $discount;
+
+        return $this;
+    }
+
+    public function getTaxFreePrice(): ?float
+    {
+        return $this->taxFreePrice;
+    }
+
+    public function setTaxFreePrice(float $taxFreePrice): self
+    {
+        $this->taxFreePrice = $taxFreePrice;
+
+        return $this;
+    }
+
+    public function getCategory(): ?ProductCategory
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?ProductCategory $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
