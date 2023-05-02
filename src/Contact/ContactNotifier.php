9e979c46caf3862023a27e0520768bca07d15495
facte sun <shems.chelgoui@gmail.com>
2023-05-02 12:03:08 +0200

    ✨ contact mailer as service

---
+++ src/Contact/ContactNotifier.php
@@ -0,0 +1,19 @@
+<?php
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class ContactNotifier {
+
+    public function __construct(private MailerInterface $mailer, private string $adminEmail) {
+    }
+
+    public function send(array $data) {
+        $email = (new Email())
+        ->To($this->adminEmail)
+        ->from($data['email'])
+        ->subject('Nouvelle demande : ' . $data['subject']->getData())
+        ->text($data['message']->getData());
+    $this->mailer->send($email);
+    }
+}
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,28 +3,22 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use ContactNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact', methods: [Request::METHOD_GET, Request::METHOD_POST])]
-    public function index(Request $request, MailerInterface $mailer): Response
+    public function index(Request $request, ContactNotifier $contactNotifier): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $email = (new Email())
-                ->To('admin@Synmfony.com')
-                ->from($form['email']->getData())
-                ->subject('Nouvelle demande : ' . $form['subject']->getData())
-                ->text($form['message']->getData());
-            $mailer->send($email);
-
+                
+            $contactNotifier->send($form->getData());
             $this->addFlash('success', 'Message sent!');
         }
         return $this->renderForm('contact/contact.html.twig', [
